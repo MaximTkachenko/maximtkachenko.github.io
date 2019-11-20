495d47a7ce37b232bfa8fefc434c98bb42203c88
mtkachenko <oblomov86@gmail.com>
2019-11-20 14:25:37 +0100

    fixed disqus

--- _site/pretzel_install.cmd
+++
@@ -1,6 +0,0 @@
-rmdir "pretzel" /s /q
-git clone -b master git@github.com:Code52/pretzel.git
-dotnet build pretzel/src/Pretzel/Pretzel.csproj -c Release
-rmdir "bin" /s /q
-xcopy "pretzel/src/Pretzel/bin/Release/netcoreapp2.2" "bin" /i /s /y
-rmdir "pretzel" /s /q
